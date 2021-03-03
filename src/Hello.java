8fd9946ca8f2001446fb202597d1907179d3b909
Marina Neumann <marinaneumann2468@yahoo.com>
2021-03-03 11:52:46 -0800

    Added Java practice with primitive data types

--- src/Hello.java
+++ src/Hello.java
@@ -1,3 +1,6 @@
+import java.math.*;
+
+
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World");
@@ -8,10 +11,21 @@ public class Hello {
         System.out.println(num4);
         int challengeNum = challenge1(num4);
         System.out.println(challengeNum);
+        practice1();
     }
 
     public static int challenge1(int number){
         int myLastOne = number -1000;
         return myLastOne;
+    }
+
+    public static void practice1(){
+        int value1 = 10000;
+        int value2 = 50000;
+        int min =  Math.min(value1, value2);
+        int max = Math.max(value1, value2);
+        System.out.println("Minimum value is: " + min);
+        System.out.println("Maximum value is:" + max);
+
     }
 }
